eb23682fc8c6183aaf6fb174fd4cab539ccba7f0
Fran Fodor <49796718+franFodor@users.noreply.github.com>
2026-09-18 07:35:57 +0200

    Initialize dev

---
+++ examples/Template/Template.ino
@@ -0,0 +1,15 @@
+/**
+ * @file Template.ino
+ * @brief Example template for Qwiic sensors
+ * @author Soldered Electronics
+ */
+
+#include "Generic-Qwiic-SOLDERED.h"
+
+void setup()
+{
+}
+
+void loop()
+{
+}
